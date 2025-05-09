fcab5d00553e5aace9651744ae043defe4e1ff0e
Snehith529 <snehith529@gmail.com>
2025-05-08 23:10:56 -0500

    Frontend integrating with Docker

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'http://localhost:8081/api',
 });
+
 
 instance.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
